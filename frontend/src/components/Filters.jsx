6cb5ef1edeb6a117a38439870e7155e5c798c4a7
Michail <pmihail734@gmail.com>
2020-04-02 16:31:14 +0300

    Основная часть фильтров

--- frontend/src/components/Filters.jsx
+++ frontend/src/components/Filters.jsx
@@ -5,13 +5,9 @@ class Filters extends React.Component {
     super(props);
 
     this.state = {
-      blockOne: false,
-      blockTwo: false,
+      blockProfessions: false,
       areas: { areas: [] },
       specializations: [],
-      filters: {
-        profession: ""
-      },
       professions: [],
       dictionaries: {
         employment: [],
@@ -20,6 +16,16 @@ class Filters extends React.Component {
         vacancy_type: [],
         vacancy_label: [],
         vacancy_search_fields: []
+      },
+      filters: {
+        specialization: "",
+        profession: "",
+        employment: "",
+        experience: "",
+        schedule: "",
+        vacancy_type: "",
+        vacancy_label: "",
+        vacancy_search_fields: ""
       }
     };
   }
@@ -30,6 +36,7 @@ class Filters extends React.Component {
     this.fetchDictionaries();
   }
 
+  ///местоположение
   async fetchAreas() {
     let response = await fetch("https://api.hh.ru/areas");
     let result = await response.json();
@@ -38,6 +45,7 @@ class Filters extends React.Component {
     });
   }
 
+  ///доп настройки
   async fetchDictionaries() {
     let response = await fetch("https://api.hh.ru/dictionaries");
     let result = await response.json();
@@ -46,6 +54,7 @@ class Filters extends React.Component {
     });
   }
 
+  ///специализация
   async fetchSpecializations() {
     let response = await fetch("https://api.hh.ru/specializations");
     let result = await response.json();
@@ -58,19 +67,39 @@ class Filters extends React.Component {
     let specializationsIndex = document.querySelector(
       ".select__specializations"
     ).value;
-    let profArray = this.state.specializations[+specializationsIndex]
+    let specializationsIndexZero = specializationsIndex.split(" ");
+    let profArray = this.state.specializations[+specializationsIndexZero[0]]
       .specializations;
     this.setState({
-      blockOne: true,
+      blockProfessions: true,
       professions: profArray
     });
   }
 
   saveData() {
-    // let specialization = document.querySelector(".select__specializations")
+    let specialization = document.querySelector(".select__specializations")
+      .value;
+    let specializationsIndexOne = specialization.split(" ");
     let profession = document.querySelector(".select__professions").value;
+    let employment = document.querySelector(".select__employment").value;
+    let experience = document.querySelector(".select__experience").value;
+    let schedule = document.querySelector(".select__schedule").value;
+    let vacancy_type = document.querySelector(".select__vacancy_type").value;
+    let vacancy_label = document.querySelector(".select__vacancy_label").value;
+    let vacancy_search_fields = document.querySelector(
+      ".select__vacancy_search_fields"
+    ).value;
+    let keyWords = document.querySelector(".keyWords").value;
     let data = {
-      profession: profession
+      specialization: specializationsIndexOne[1],
+      profession,
+      employment,
+      experience,
+      schedule,
+      vacancy_type,
+      vacancy_label,
+      vacancy_search_fields,
+      keyWords
     };
     this.setState({
       filters: data
@@ -78,15 +107,22 @@ class Filters extends React.Component {
   }
 
   render() {
-    console.log(this.state);
+    console.log(this.state.filters);
     return (
       <div>
+        <h3>Ключевые слова</h3>
+        <input type="text" name="keyWords" className="keyWords" />
+        <select name="" className="select__vacancy_search_fields">
+          {this.state.dictionaries.vacancy_search_fields.map(item => {
+            return <option value={item.id}>{item.name}</option>;
+          })}
+        </select>
         <div className="block__specializations">
           <h3>Выбор специализации</h3>
           <select className="select__specializations">
             {this.state.specializations.map((item, index) => {
               return (
-                <option value={index} name={item.id}>
+                <option value={`${index} ${item.id}`} name={item.id}>
                   {item.name}
                 </option>
               );
@@ -94,7 +130,7 @@ class Filters extends React.Component {
           </select>
           <button onClick={() => this.professions()}>Подтвердить</button>
         </div>
-        {this.state.blockOne ? (
+        {this.state.blockProfessions ? (
           <div className="block__profissions">
             <h3>Выбор профессии</h3>
             <select className="select__professions">
@@ -102,48 +138,42 @@ class Filters extends React.Component {
                 return <option value={item.id}>{item.name}</option>;
               })}
             </select>
-            <button onClick={() => this.saveData()}>Подтвердить</button>
           </div>
         ) : null}
 
         <div className="block__dictionaries">
           <h3>Tип занятости</h3>
-          <select name="" id="">
+          <select name="" className="select__employment">
             {this.state.dictionaries.employment.map(item => {
-              return <option>{item.name}</option>;
+              return <option value={item.id}>{item.name}</option>;
             })}
           </select>
           <h3>Опыт работы</h3>
-          <select name="" id="">
+          <select name="" className="select__experience">
             {this.state.dictionaries.experience.map(item => {
-              return <option>{item.name}</option>;
+              return <option value={item.id}>{item.name}</option>;
             })}
           </select>
           <h3>График работы</h3>
-          <select name="" id="">
+          <select name="" className="select__schedule">
             {this.state.dictionaries.schedule.map(item => {
-              return <option>{item.name}</option>;
+              return <option value={item.id}>{item.name}</option>;
             })}
           </select>
           <h3>Tип вакансии</h3>
-          <select name="" id="">
+          <select name="" className="select__vacancy_type">
             {this.state.dictionaries.vacancy_type.map(item => {
-              return <option>{item.name}</option>;
+              return <option value={item.id}>{item.name}</option>;
             })}
           </select>
           <h3>Метки вакансии</h3>
-          <select name="" id="">
+          <select name="" className="select__vacancy_label">
             {this.state.dictionaries.vacancy_label.map(item => {
-              return <option>{item.name}</option>;
-            })}
-          </select>
-          <h3>Метки вакансии</h3>
-          <select name="" id="">
-            {this.state.dictionaries.vacancy_search_fields.map(item => {
-              return <option>{item.name}</option>;
+              return <option value={item.id}>{item.name}</option>;
             })}
           </select>
         </div>
+        <button onClick={() => this.saveData()}>Сохранить</button>
       </div>
     );
   }
