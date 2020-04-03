6895899f58f0854935bb49399632a9fa313432b1
Alex <alex90prus@yandex.ru>
2020-04-03 17:06:21 +0300

    'styles3'

---
+++ frontend/src/components/pages/Main/Filters/filterComponentsAndFetch/WageLevel.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class WageLevel extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="input-field col s6">
+          <h4>Уровень заработной платы</h4>
+          <input type="text" placeholder="RUB" className="WageLevel" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default WageLevel;
