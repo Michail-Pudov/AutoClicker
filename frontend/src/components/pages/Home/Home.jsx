01dc280eb3c8b08f43cbdadaf7f3e53cd6dcb161
Alex90rus <alex90prus@yandex.ru>
2020-04-09 20:35:52 +0300

    validation

--- frontend/src/components/pages/Home/Home.jsx
+++ frontend/src/components/pages/Home/Home.jsx
@@ -5,11 +5,15 @@ import { Link } from 'react-router-dom';
 class Home extends Component {
   render() {
     return (
-      <div className="row home white-text">
-        <h1 className="col s8 offset-l1" style={{ marginTop: '80px' }}>Найти работу!</h1>
-        <h2 className="col s8 offset-l2">Быстро!</h2>
-        <div className="col s8 offset-s3">
-          <Link to="/search" style={{ marginTop: '60px' }} className="pulse btn-floating btn-large">
+      <div className="home white-text">
+        <div className="borderTextH1">
+          Найти работу!
+        </div>
+        <div className="borderTextH2">
+          Быстро!
+        </div>
+        <div className="buttonSearchOfHome">
+          <Link to="/search" className="pulse btn-floating btn-large">
             <i className="material-icons">search</i>
           </Link>
         </div>
