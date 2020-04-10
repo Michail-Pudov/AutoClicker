fbd8cdbd5bc564062b30bdc522f5561f8272e993
Alex <alex90prus@yandex.ru>
2020-04-10 13:49:18 +0300

    'final-style-homepage'

--- frontend/src/components/pages/Home/Home.jsx
+++ frontend/src/components/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 class Home extends Component {
   render() {
     return (
-      <div className="home white-text">
+      <div className="home">
         <div className="borderTextH1">
           Найти работу!
         </div>
