c56315a066ea7488c610f6a2ae225be16dfb963e
Michail <pmihail734@gmail.com>
2020-04-13 16:11:38 +0300

    settings

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,7 @@ const indexRouter = require("./routes/index");
 const accountRouter = require("./routes/account");
 const config = require("config");
 const app = express();
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
