3e6ca4950a01d99785a1c2f76c12040cc7c6b37e
Michail <pmihail734@gmail.com>
2020-04-13 15:48:59 +0300

    ...

--- frontend/src/allFetch/vacansiesFetch.js
+++ frontend/src/allFetch/vacansiesFetch.js
@@ -1,21 +1,34 @@
 export const vacansiesFetch = async payload => {
-  let response = await fetch(
-    `https://api.hh.ru/vacancies?salary=${
-      payload.WageLevel ? payload.WageLevel : 10000
-    }&employment=${payload.employment}&experience=${
-      payload.experience
-    }&profession=${payload.profession}&schedule=${
-      payload.schedule
-    }&specialization=${payload.specialization}&vacancy_label=${
-      payload.vacancy_label
-    }&vacancy_search_fields=${payload.vacancy_search_fields}&vacancy_type=${
-      payload.vacancy_type
-    }&text=${
-      payload.keyWords
-    }&per_page=100&area=1&vacancy_search_order=publication_time`
-  );
+  let test = `https://api.hh.ru/vacancies?${
+    payload.WageLevel ? `salary=${payload.WageLevel}` : ""
+  }&${payload.employment ? `employment=${payload.employment}` : ""}
+  &${payload.experience ? `experience=${payload.experience}` : ""}&profession=${
+    payload.profession
+  }&${payload.schedule ? `schedule=${payload.schedule}` : ""}&specialization=${
+    payload.specialization
+  }&${payload.vacancy_label ? `vacancy_label=${payload.vacancy_label}` : ""}&${
+    payload.vacancy_search_fields
+      ? `vacancy_search_fields=${payload.vacancy_search_fields}`
+      : ""
+  }&${payload.vacancy_type ? `vacancy_type=${payload.vacancy_type}` : ""}&${
+    payload.keyWords ? `text=${payload.keyWords}` : ""
+  }&per_page=100&area=1&vacancy_search_order=publication_time`;
+  let response = await fetch(test);
 
-  
+  let old = `https://api.hh.ru/vacancies?salary=${
+    payload.WageLevel ? payload.WageLevel : 10000
+  }&employment=${payload.employment}&experience=${
+    payload.experience
+  }&profession=${payload.profession}&schedule=${
+    payload.schedule
+  }&specialization=${payload.specialization}&vacancy_label=${
+    payload.vacancy_label
+  }&vacancy_search_fields=${payload.vacancy_search_fields}&vacancy_type=${
+    payload.vacancy_type
+  }&text=${
+    payload.keyWords
+  }&per_page=100&area=1&vacancy_search_order=publication_time`;
+
   let result = await response.json();
   return result.items;
 };
